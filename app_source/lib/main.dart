dd62b5dd60ebf4a82ae307647b9786c9f1526b90
mayman007 <medoems2210@gmail.com>
2025-09-15 20:59:11 +0300

    added appcheck

--- app_source/lib/main.dart
+++ app_source/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:athr/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'pages/login_page.dart';
 import 'pages/signup_page.dart';
 import 'pages/dashboard_page.dart';
@@ -11,6 +11,11 @@ import 'pages/admin_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAppCheck.instance.activate(
+    webProvider: ReCaptchaV3Provider(
+      '6LdynMorAAAAAPo2PAi3G5s3IHz2Iqpw2R2TnzdJ',
+    ),
+  );
   runApp(AthrApp());
 }
 
